873d5163efedee2342b31b356edb35a0972bd82c
D. RikaZ <119458243+dzakkirsolihin@users.noreply.github.com>
2024-06-11 17:23:27 +0700

    initial commit

---
+++ src/main/java/com/mycompany/perseablue/PerSeaBlue.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.perseablue;
+
+/**
+ *
+ * @author WIN11
+ */
+public class PerSeaBlue {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
